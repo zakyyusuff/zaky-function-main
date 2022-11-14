f5601231834daf994041d3ca188cd0b7051b49b0
zakyyusuff <zakymuhamamdyusuf@gmail.com>
2022-11-14 13:39:04 +0700

    hapus testscript

--- mainfunction.py
+++ mainfunction.py
@@ -1,6 +1,5 @@
 import pytest
 
-@pytest.mark.parametrize("a, b, c", [(10,20, 30), (20,40,60), (11,22,33)])
 def test_add(a, b, c):
     res = add(a, b)
     assert res == c
@@ -29,22 +28,18 @@ def dikalikan_dua(x):
 def dibagi_dua(x):
     return divide(x, 2)
 
-@pytest.mark.easy_operation
 def test_add():
     # penjumlahan
     assert add(4, 8) == 12
 
-@pytest.mark.easy_operation
 def test_subtract():
     # pengurangan
     assert subtract(3, 6) == -3
 
-@pytest.mark.difficult_operation
 def test_multiply():
     # perkalian
     assert multiply(5, 5) == 25
 
-@pytest.mark.difficult_operation
 def test_divide():
     # pembagian
     assert divide(21, 3) == 7
