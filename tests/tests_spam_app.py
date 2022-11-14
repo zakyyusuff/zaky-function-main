a3e0784ddd4e9cd5402a9ed0c6154147479c67f6
zakyyusuff <zakymuhamamdyusuf@gmail.com>
2022-11-14 12:26:41 +0700

    revisi test

--- tests/tests_spam_app.py
+++ tests/tests_spam_app.py
@@ -2,12 +2,10 @@ import mainfunction as min
 from mainfunction import numbers
 
 class TestApp:
-    # @pytest.mark.easy_operation
     def test_kali(self, numbers):
         res = min.dikalikan_dua(numbers[0])
         assert res == numbers[1]
 
-    # @pytest.mark.easy_operation
     def test_bagi(self, numbers):
         res = min.dibagi_dua(numbers[1])
         assert res == numbers[0]
